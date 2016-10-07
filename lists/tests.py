293887a5807f02bc6413f97480f3708c73545e0f
WillJCJ <willjcjohnson@gmail.com>
2016-10-07 09:18:47 +0100

    New item appears in list

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -16,4 +16,18 @@ class SmokeTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
